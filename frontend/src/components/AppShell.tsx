be439ecef757c6e51a277aea2230c3116b0bd0eb
Romaric Philogène <rphilogene@qovery.com>
2023-12-15 21:29:28 +0100

    wip: front catalog services

--- frontend/src/components/AppShell.tsx
+++ frontend/src/components/AppShell.tsx
@@ -160,7 +160,7 @@ export default function AppShell() {
         </Transition.Root>
 
         {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
             <div className="flex h-16 shrink-0 items-center">
